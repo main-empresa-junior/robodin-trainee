f01d440c32a037a5d4e5c19c74cb5ca9cb922eb9
Tobias Carvalho <83415221+TobiasCarvalho@users.noreply.github.com>
2026-03-23 18:11:58 -0300

    Add files via upload

---
+++ site_robodin/robodin/script.js
@@ -0,0 +1,23 @@
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0:{
+        slidesPerView: 3
+    }
+  }
+});
